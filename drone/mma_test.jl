6cad0b5b573044cbba568ec8625c677458eda903
ge65luz <ge65luz@mytum.de>
2024-07-09 14:43:21 +0200

    test on others

--- drone/mma_test.jl
+++ drone/mma_test.jl
@@ -3,7 +3,7 @@ using DojoEnvironments
 using LinearAlgebra
 using Rotations
 
-HORIZON = 1000
+HORIZON = 2000
 quadrotor_env = get_environment(:quadrotor_waypoint; horizon=HORIZON)
 ref_position_xyz_world = [0;0;0]
 next_waypoint = 1
@@ -12,15 +12,15 @@ state_traj = zeros(size(get_state(quadrotor_env))[1], HORIZON)
 reference_traj = zeros(4, HORIZON)
 plot_dict = Dict("state_traj" => state_traj, "reference_traj" => reference_traj)
 
-function get_transformation_body_to_world(x, y, z, roll, pitch, yaw)
-    R = RotXYZ(roll, pitch, yaw)
-    R_matrix = convert(Array{Float64, 2}, R) # Convert to matrix
-    println("R_matrix: ", R_matrix)
+# function get_transformation_body_to_world(x, y, z, roll, pitch, yaw)
+#     R = RotXYZ(roll, pitch, yaw)
+#     R_matrix = convert(Array{Float64, 2}, R) # Convert to matrix
+#     println("R_matrix: ", R_matrix)
     
-    position_w = [x, y, z]
-    T_world_to_body = [R_matrix' -R_matrix' * position_w; 0 0 0 1]
-    return T_world_to_body
-end
+#     position_w = [x, y, z]
+#     T_world_to_body = [R_matrix' -R_matrix' * position_w; 0 0 0 1]
+#     return T_world_to_body
+# end
 
 function MMA!(roll, pitch, yaw, thrust)
     u = zeros(4)
@@ -50,21 +50,21 @@ function position_to_quadrotor_orientation_controller(environment, k)
     roll, pitch, yaw = get_state(environment)[4:6]
     linear_velocity = get_state(environment)[7:9]
     
-    transformation_b_w = get_transformation_body_to_world(position[1], position[2], position[3], roll, pitch, yaw) 
-    println("transformation_b_w: ", transformation_b_w)
-    ref_position_b = transformation_b_w * [ref_position_xyz_world;1]
-    ref_position_b = ref_position_b[1:3]
-    ref_position_b = ref_position_xyz_world
-    println("current position: , $position")
-    plot_dict["state_traj"][:, k] = get_state(environment)
-    println("ref_position_b: ", ref_position_b)  
+    # transformation_b_w = get_transformation_body_to_world(position[1], position[2], position[3], roll, pitch, yaw) 
+    # println("transformation_b_w: ", transformation_b_w)
+    # ref_position_b = transformation_b_w * [ref_position_xyz_world;1]
+    # ref_position_b = ref_position_b[1:3]
+    # ref_position_b = ref_position_xyz_world
+    # println("current position: , $position")
+    # plot_dict["state_traj"][:, k] = get_state(environment)
+    # println("ref_position_b: ", ref_position_b)  
    
     K_p_xy_roll = K_p_xy_pitch = 0.04
     K_d_xy_roll = K_d_xy_pitch = 0.1
    
-    roll_ref = -(K_p_xy_roll * (ref_position_b[2] - position[2]) + K_d_xy_roll * (0 - linear_velocity[2]))
-    pitch_ref = K_p_xy_pitch * (ref_position_b[1] - position[1]) + K_d_xy_pitch * (0 - linear_velocity[1])
-    plot_dict["reference_traj"][1:3, k] = [roll_ref, pitch_ref, ref_position_b[2]-position[2]]
+    roll_ref = -(K_p_xy_roll * (ref_position_xyz_world[2] - position[2]) + K_d_xy_roll * (0 - linear_velocity[2]))
+    pitch_ref = K_p_xy_pitch * (ref_position_xyz_world[1] - position[1]) + K_d_xy_pitch * (0 - linear_velocity[1])
+    plot_dict["reference_traj"][1:3, k] = [roll_ref, pitch_ref, ref_position_xyz_world[2]-position[2]]
    
     return roll_ref, pitch_ref
 end 
@@ -75,7 +75,7 @@ function cascade_controller!(environment, k)
     global ref_position_xyz_world
     # get the current system values
     roll, pitch, yaw, altitude = sensing_and_estimation(environment)
-    println("state: $(sensing_and_estimation(environment))")
+    println("states : $(sensing_and_estimation(environment))")
     # set some reference - TODO - make this to be set from outside or smth
     #roll_ref, pitch_ref, yaw_ref, altitude_ref = [0.0, 0.0, 1.57, 1]
     roll_ref, pitch_ref, = position_to_quadrotor_orientation_controller(environment, k)
@@ -103,16 +103,15 @@ function cascade_controller!(environment, k)
     pitch_cntrl = K_p_pitch * (pitch_ref - pitch) + K_d_pitch * (0 - get_state(environment)[11])
     yaw_cntrl = K_p_yaw * (yaw_ref - yaw) + K_d_yaw * (0 - get_state(environment)[12])
     thrust = K_p_thrust * (altitude_ref - altitude) + K_d_thrust * (0 - get_state(environment)[9])+ thrust_feedforward
-
+    println("roll_cntrl: ", roll_cntrl, " pitch_cntrl: ", pitch_cntrl, " yaw_cntrl: ", yaw_cntrl, " thrust: ", thrust)
     u = MMA!(roll_cntrl, pitch_cntrl, yaw_cntrl, thrust)
     set_input!(environment, u)
 
 end
 
 function controller!(environment, k)
-    #cascade_controller(environment, k)
     println("k: ", k)
-    fly_through_waypoints_controller!(environment, k)
+    fly_through_waypoints_controller!(environment, k) #needed here to define the goal position
 end
 
 function default_controller!(environment, k)
@@ -140,7 +139,7 @@ function fly_through_waypoints_controller!(environment, k)
     cascade_controller!(environment, k)
 end
 
-initialize!(quadrotor_env, :quadrotor; body_orientation=Dojo.RotZ(0))
+initialize!(quadrotor_env, :quadrotor)
 simulate!(quadrotor_env, controller!; record=true)
 
 vis = visualize(quadrotor_env)
